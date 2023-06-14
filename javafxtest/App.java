d8ad6b8ce68c5ca7cede10d757acecf7c987ae00
nguyet93 <thanhtung.nguyen1990@gmail.com>
2023-06-14 13:21:10 -0400

    commit by thanh

---
+++ javafxtest/App.java
@@ -0,0 +1,49 @@
+package org.openjfx.fxtest;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		//Stage stage = new Stage();
+		Group root = new Group();
+		Scene scene = new Scene(root, Color.BLACK);
+		
+		//Image icon = new Image("icon.png");
+		//stage.getIcons().add(icon);
+		stage.setTitle("Stage Demo Program");
+		stage.setHeight(420);
+		stage.setWidth(420);
+		stage.setResizable(false);
+//		stage.setX(50);
+//		stage.setY(50);
+		stage.setFullScreen(true);
+		stage.setFullScreenExitHint("YOU CAN'T ESCAPE, Press X to exit");
+		stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("X"));
+		
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+
+    public static void main(String[] args) {
+        launch();
+        System.out.print("Hello!");
+        System.out.print("Hello123!");
+    }
+
+}
